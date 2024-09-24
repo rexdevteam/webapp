d2829397d47913684cfd53bda6c64dbd94ef784d
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-09-24 01:32:16 +0100

    fix import error

--- backend/app/core/api/routes/location.py
+++ backend/app/core/api/routes/location.py
@@ -1,18 +1,18 @@
 from flask import request
 
-from . import api
+from . import api_bp
 from ..controllers import LocationController
 
 
-@api.route('/countries', methods=['GET'])
+@api_bp.route('/countries', methods=['GET'])
 def get_countries():
     return LocationController.get_supported_countries()
 
 
-@api.route('/states', methods=['POST'])
+@api_bp.route('/states', methods=['POST'])
 def get_states():
     return LocationController.get_supported_country_states()
 
-# @api.route('/states/lga', methods=['POST'])
+# @api_bp.route('/states/lga', methods=['POST'])
 # def naija_states_lga():
 #     return LocationController.get_naija_state_lga()
